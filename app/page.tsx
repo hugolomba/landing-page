40c9bfa59db97c4f68407245e1e6b36f9081cdde
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2026-01-16 14:28:01 +0000

    fixed navbar

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import ProjectsSection from "../components/ui/projects";
 import Testimonials from "../components/ui/testimonials";
 import CTA from "../components/ui/CTA";
 import Insights from "../components/ui/insights";
-import Footer from "../components/ui/footer";
 
 export default function Home() {
   return (
