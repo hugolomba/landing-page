c947b9b2cd94f86b7ab43bd5bc040d7786f40512
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2026-01-19 16:38:08 +0000

    add google verification

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://supernova.hugo-miranda.dev",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://supernova.hugo-miranda.dev/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
